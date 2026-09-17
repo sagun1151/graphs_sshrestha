296d800f75a8ffccc68cde0274664690b9c0a8e1
Sagun Shrestha <sshrestha426699@mail.com>
2026-09-16 19:13:03 -0600

    creation of file structure and text.py code

---
+++ src/test.py
@@ -0,0 +1,27 @@
+from graphs_sshrestha import sp
+import sys
+
+if __name__ == '__main__':
+    
+    if len(sys.argv) != 2:
+        print(f'Use: {sys.argv[0]} graph_file')
+        sys.exit(1)
+
+    graph = {}
+    with open(sys.argv[1], 'rt') as f:
+        for line in f:
+            line = line.strip()
+            s, d, w = line.split()
+            s = int(s)
+            d = int(d)
+            w = int(w)
+            if s not in graph:
+                graph[s] = {}
+            graph[s][d] = w
+    
+    s = 0
+    dist, path = sp.dijkstra(graph, s)
+    print(f'Shortest distances from {s}:')
+    print(dist)
+    for d in path: 
+        print(f'spf to {d}: {path[d]}')
